d3ceee50b61d1edcdf01f10435c7fb3f9d3cf414
Luan-Felipe <lfps1999@gmail.com>
2021-02-21 20:38:29 -0300

    Create all routes for tasks and validate now the connection to database

--- back/src/index.ts
+++ back/src/index.ts
@@ -1,15 +1,24 @@
 import express from "express";
 import "reflect-metadata";
 
+import db from "./config/database";
 import routes from "./routes";
 
-const app = express();
-const port = 3333;
-const baseURL = `http://localhost:${port}`;
+const validateConnection = () => {
+  db.then((t) => {
+    const app = express();
+    const port = 3333;
+    const baseURL = `http://localhost:${port}`;
 
-app.use(express.json());
-app.use(routes);
+    app.use(express.json());
+    app.use(routes);
 
-app.listen(port, () => {
-  console.log(`Server has started at ${baseURL}`);
-});
+    app.listen(port, () => {
+      console.log(`Server has started at ${baseURL}`);
+    });
+  }).catch((err) => {
+    console.error(err);
+  });
+};
+
+validateConnection();
--- back/src/routes/tasks.routes.ts
+++ back/src/routes/tasks.routes.ts
@@ -1,9 +1,74 @@
 import express from "express";
+import { getRepository } from "typeorm";
+import { v4 } from "uuid";
+
+import Task from "../models/task";
 
 const routes = express();
 
-routes.get("/", (request, response) => {
-  response.json({ message: "tasks!" });
+routes.get("/", async (request, response) => {
+  const taskRepository = getRepository(Task);
+
+  const tasks = (await taskRepository.find({
+    order: { created_at: "ASC" },
+  })) as Task[];
+
+  response.json(tasks);
+});
+
+routes.post("/", async (request, response) => {
+  const { Title } = request.body;
+
+  const taskRepository = getRepository(Task);
+
+  const newTask = taskRepository.create({
+    id: v4(),
+    Title,
+    Done: false,
+  });
+
+  await taskRepository.save(newTask);
+
+  response.json(newTask);
+});
+
+routes.put("/switch/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const taskRepository = getRepository(Task);
+
+  const task = (await taskRepository.findOne(id)) as Task;
+
+  task.Done = !task.Done;
+
+  await taskRepository.save(task);
+
+  response.json(task);
+});
+
+routes.put("/:id", async (request, response) => {
+  const { id } = request.params;
+  const { Title } = request.body;
+
+  const taskRepository = getRepository(Task);
+
+  const task = (await taskRepository.findOne(id)) as Task;
+
+  task.Title = Title;
+
+  await taskRepository.save(task);
+
+  response.json(task);
+});
+
+routes.delete("/:id", async (request, response) => {
+  const { id } = request.params;
+
+  const taskRepository = getRepository(Task);
+
+  await taskRepository.delete(id);
+
+  response.json({ message: "Task deleted!" });
 });
 
 export default routes;
